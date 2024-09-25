79f7a5bbc8dab4ded17662b707533aab4f26cc6d
Smit Patel <sbp1825@gmail.com>
2024-09-25 13:47:59 +0530

    Tabs component

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,30 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
-export const Tabs = ({ children, initialSelectedTab }) => {
-  const tabs = React.Children.map(children, (child) => ({
-    tabId: child.props.tabId,
-    label: child.props.label,
-    content: child.props.children,
-  }));
-
-  const [activeTabId, setActiveTabId] = useState(
-    initialSelectedTab ?? tabs[0].tabId
-  );
-
-  useEffect(() => {
-    setActiveTabId(initialSelectedTab);
-  }, [initialSelectedTab]);
-
+export const Tabs = ({ initialSelectedTab }) => {
   return (
-    <div>
-      <div>
-        {tabs.map((tab) => (
-          <button key={tab.tabId} onClick={() => setActiveTabId(tab.tabId)}>
-            {tab.label}
-          </button>
-        ))}
-      </div>
-      <div>{tabs.find((tab) => tab.tabId === activeTabId).content}</div>
-    </div>
+    <div></div>
   );
 };
